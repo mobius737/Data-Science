c97dcb2c36720a38542769f33b4c9540ff879566
stephen.long <stephen.long@capspire.com>
2021-06-21 10:51:34 -0500

    initial commit

---
+++ VSRepos/Test/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,20 @@
+# create the initial variables below
+age=28
+sex=0
+bmi=26.2
+num_of_children=3
+smoker=0
+
+# Add insurance estimate formula below
+insurance_cost=250*age-128*sex+370*bmi+425*num_of_children+24000*smoker-12500
+
+# Age Factor
+print('This person''s insurance cost is {0}'.format(insurance_cost))
+
+# BMI Factor
+
+
+# Male vs. Female Factor
+
+
+# Extra Practice
---
+++ VSRepos/Test/PythonApplication1/travel.py
@@ -0,0 +1,6 @@
+# Write your code below:
+def trip_planner(first_destination, second_destination,final_destination="Codecademy HQ"):
+  print('Here is what your trip will look like!')
+  print("First, we will stop in {0}, then {1}, and lastly {2}".format(first_destination,second_destination,final_destination))
+
+trip_planner('here','there','everywhere')
